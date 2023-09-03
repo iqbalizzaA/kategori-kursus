0d3ae9fb4cc924fb62a3ca9b866b0aecc9193bea
Iqbal Izza Addaviqi <100308152+iqbalizzaA@users.noreply.github.com>
2023-09-04 02:29:52 +0700

    membuat kategori kursus-magang tugas1

--- app/page.js
+++ app/page.js
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+import Image from "next/image";
+import gambar5 from "../app/icon/ic-graphic.svg";
+import gambar4 from "../app/icon/ic-health.svg";
+import gambar1 from "../app/icon/ic-marketing.svg";
+import gambar3 from "../app/icon/ic-webdev.svg";
+import gambar2 from "../app/icon/ic-writter.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <div className="mt-8 container mx-auto">
+        <div className="text-center">
+          <span className="text-gray-600 text-3xl font-bold">
+            Kategori Kursus
+          </span>
+        </div>
+        <div className="text-center">
+          <span className="text-black">
+            Lihat selengkapnya dan pilih sesuai minatmu
+          </span>
+        </div>
+        <div className="mt-4 grid grid-cols-1 p-6 md:grid-cols-5 gap-5">
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar1}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Digital Marketing
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar2}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Content Writter
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar3}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Web Development
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar4}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Self Development
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar5}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Graphic Design
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar1}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Digital Marketing
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar2}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Content Writter
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar3}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Web Development
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar4}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Self Development
+            </span>
+          </div>
+
+          <div className="drop-shadow shadow-xl border p-4 rounded-2xl">
+            <div className="flex justify-center mb-3">
+              <Image
+                src={gambar5}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+            </div>
+            <span className="text-black text-center font-bold flex justify-center">
+              Graphic Design
+            </span>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-8 mb-5 container mx-auto flex justify-center">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className=" text-center border-2 border-gray-400 text-gray-500 rounded-2xl px-12 py-2 inline-block font-semibold hover:bg-gray-600 hover:text-white"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          Lihat Semua Kategori
         </a>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
